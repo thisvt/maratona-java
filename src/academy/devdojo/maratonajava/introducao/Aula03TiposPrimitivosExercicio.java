ea8d67405c9dd02078407ce10fa1e4478e97798c
thisvt <victortsantanar2@gmail.com>
2025-05-27 20:08:31 -0300

    docs: adição do readme.md

--- src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Aula03TiposPrimitivosExercicio {
     public static void main(String[] args) {
         String name = "Victor";
-        String adress = "Rua Lucena Roriz" ;
+        String adress = "Rua Lucena " ;
         double wage = 1561.00 ;
         String date = "05/06/2025" ;
         System.out.println("Eu: " + name + "," + " morando no endereço " + adress + "," + " confirmo que recebi o salário de " + wage + "," + " na data " + date  );
