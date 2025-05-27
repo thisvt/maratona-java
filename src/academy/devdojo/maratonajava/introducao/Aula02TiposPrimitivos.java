06af46f2723f5d7270f87e56e78edeadd5f1cf95
thisvt <victortsantanar2@gmail.com>
2025-05-27 19:44:34 -0300

    Arquivos iniciais de Estudo

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+        // byte, short, int, long, flot, double, char, boolean
+        int age = 18 ;
+        long numeroGrande = 100000000 ;
+        double wageDouble = 2000.0d ;
+        float wageFloat = 2500.0f ;
+        byte ageByte = 18 ;
+        short ageShort = 18 ;
+        boolean verdadeiro = true ;
+        boolean falso = false ;
+        char caractere = 'M' ;
+        String nome = "Victor";
+        System.out.println("A idade é : " + age + " anos") ;
+        System.out.println(falso);
+        System.out.println(verdadeiro);
+        System.out.println("char " + caractere);
+        System.out.println("nome: "+ nome);
+
+
+
+
+
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.time.LocalDate;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Victor";
+        String adress = "Rua Lucena Roriz" ;
+        double wage = 1561.00 ;
+        String date = "05/06/2025" ;
+        System.out.println("Eu: " + name + "," + " morando no endereço " + adress + "," + " confirmo que recebi o salário de " + wage + "," + " na data " + date  );
+
+
+
+
+
+
+
+
+
+
+    }
+}
